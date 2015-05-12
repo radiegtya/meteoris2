cbd26ed277515483ac3413a1d6577c0384c84ca3
sme001 <sme001@www.sme001.com>
2015-05-12 17:58:55 +0800

    add i18n to meteoris

--- client/templates/basic/header.js
+++ client/templates/basic/header.js
@@ -1,4 +1,10 @@
 Template.templateBasicHeader.events({
+    'click a.lang':function(e){
+	lang='undefined';
+	$this=$(e.target);
+	lang=$this.data("lang");
+	TAPi18n.setLanguage(lang);
+    },
     'click #btnLogout': function() {
         Meteor.logout(function(){
             Router.go('usersLogin');
